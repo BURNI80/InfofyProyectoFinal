e85ce6926bdfb1bde7b4815349e7f8c67350a472
BURNI80 <alvarogutierrezc6@gmail.com>
2022-11-18 23:33:01 +0100

    Cambios en URL

--- src/Global/Global.js
+++ src/Global/Global.js
@@ -3,7 +3,7 @@ var Global = {
 	songLimit: 100,
 	access_token: "",
 	refresh_token: "",
-	redirect_uri : "https://infofyproyectofinal-production.up.railway.app",
+	redirect_uri : "https://infofy-agc.up.railway.app",
 	client_id : "c33ccc8f8e0b4de2ac731fe177912cce",
 	client_secret : "c4fb592872a94a3984bb89fea01e1402",
 	scopes : "user-read-private user-read-email playlist-read-private"
